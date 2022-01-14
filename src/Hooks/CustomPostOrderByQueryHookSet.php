7b487109744396ccb95a49cdc3cd0fc7257003fe
leoloso <leo@getpop.org>
2022-01-14 06:55:28 +0000

    Merge pull request #1356 from leoloso/Create-root-wp,-move-there-TranslationWP,-HooksWP-and-RoutingWP
    Create RootWP, move there TranslationWP, HooksWP and RoutingWP

--- src/Hooks/CustomPostOrderByQueryHookSet.php
+++ src/Hooks/CustomPostOrderByQueryHookSet.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 
 namespace PoPWPSchema\CustomPosts\Hooks;
 
-use PoP\BasicService\AbstractHookSet;
+use PoP\Root\App;
+use PoP\Root\Hooks\AbstractHookSet;
 use PoPSchema\CustomPostsWP\TypeAPIs\AbstractCustomPostTypeAPI;
 use PoPWPSchema\CustomPosts\Constants\CustomPostOrderBy;
 
@@ -12,7 +13,7 @@ class CustomPostOrderByQueryHookSet extends AbstractHookSet
 {
     protected function init(): void
     {
-        $this->getHooksAPI()->addFilter(
+        App::addFilter(
             AbstractCustomPostTypeAPI::HOOK_ORDERBY_QUERY_ARG_VALUE,
             [$this, 'getOrderByQueryArgValue']
         );
--- src/SchemaHooks/FilterCustomPostStatusEnumTypeHookSet.php
+++ src/SchemaHooks/FilterCustomPostStatusEnumTypeHookSet.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace PoPWPSchema\CustomPosts\SchemaHooks;
 
+use PoP\Root\App;
 use PoP\ComponentModel\TypeResolvers\EnumType\EnumTypeResolverInterface;
 use PoP\ComponentModel\TypeResolvers\EnumType\HookNames;
-use PoP\BasicService\AbstractHookSet;
+use PoP\Root\Hooks\AbstractHookSet;
 use PoPSchema\CustomPosts\TypeResolvers\EnumType\FilterCustomPostStatusEnumTypeResolver;
 use PoPWPSchema\CustomPosts\Enums\CustomPostStatus;
 
@@ -14,19 +15,19 @@ class FilterCustomPostStatusEnumTypeHookSet extends AbstractHookSet
 {
     protected function init(): void
     {
-        $this->getHooksAPI()->addFilter(
+        App::addFilter(
             HookNames::ENUM_VALUES,
             [$this, 'getEnumValues'],
             10,
             2
         );
-        $this->getHooksAPI()->addFilter(
+        App::addFilter(
             HookNames::ADMIN_ENUM_VALUES,
             [$this, 'getAdminEnumValues'],
             10,
             2
         );
-        $this->getHooksAPI()->addFilter(
+        App::addFilter(
             HookNames::ENUM_VALUE_DESCRIPTION,
             [$this, 'getEnumValueDescription'],
             10,
